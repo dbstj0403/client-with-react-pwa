a07559d5355a6cc696a53464203fa2c3a9412287
yongun2 <djkye0001@naver.com>
2023-08-24 17:31:43 +0900

    [FEAT] pwa 설정

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyle';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
