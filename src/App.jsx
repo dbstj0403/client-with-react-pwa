b8dc7e0b1e39b66e3aa9eba077cc2ae63e78f453
yongun2 <djkye0001@naver.com>
2023-08-26 00:32:34 +0900

    [FEAT] 중앙무대 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
 import { Home } from './pages/Home';
+import CentralStage from './pages/CentralStage';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/stage" element={<CentralStage />} />
       </Route>
     </Routes>
   );
