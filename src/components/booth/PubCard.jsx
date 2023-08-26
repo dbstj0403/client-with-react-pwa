bc20536776ed7ada7540a696536b8e23bbd972d1
brian <90752841+wokbjso@users.noreply.github.com>
2023-08-26 17:37:53 +0900

    Fixed: 주점 부스 페이지 디자인 GUI에 맞춰 수정

--- src/components/booth/PubCard.jsx
+++ src/components/booth/PubCard.jsx
@@ -5,47 +5,91 @@ export default function PubCard({ data }) {
     <PubCardWrapper>
       <PubCardImage alt={'부스카드 이미지'} src={data.image} />
       <PubCardTextWrapper>
-        <span>{data.owns}</span>
-        <span>Page {data.page}</span>
-        <span>{data.mainMenu}</span>
-        <p>{data.introduction}</p>
+        <PubOwner>{data.owns}</PubOwner>
+        <PubPosition>
+          <PubPage isPageOne={data.page === 1}>
+            <span>Page {data.page}</span>
+          </PubPage>
+          <DevideCircle />
+          <span>{data.position}</span>
+        </PubPosition>
+        <PubMainMenu>{data.mainMenu}</PubMainMenu>
+        <PubIntroduce>{data.introduction}</PubIntroduce>
       </PubCardTextWrapper>
     </PubCardWrapper>
   );
 }
 
 const PubCardWrapper = styled.div`
-  background-color: rgba(0, 0, 0, 0.05);
-  padding: 2.1rem;
+  padding: 2.4rem 0;
   height: 17rem;
   display: flex;
-  margin: 0.9rem 0;
+  border-bottom: 1px solid #e6e6e6;
 `;
 
 const PubCardImage = styled.img`
-  width: 12.8rem;
-  height: 9.4rem;
+  width: 11.6rem;
+  height: 8rem;
 `;
 
 const PubCardTextWrapper = styled.div`
   display: flex;
-  margin-left: 2.1rem;
+  margin-left: 1.2rem;
   flex-direction: column;
-  span {
-    margin-bottom: 0.7rem;
-  }
-  span:first-child {
-    ${(props) => props.theme.fontStyles.subFont2}
-  }
-  span:nth-child(2),
-  :nth-child(3),
-  :last-child {
-    ${(props) => props.theme.fontStyles.subFont3}
-  }
-  span:nth-child(3) {
-    font-weight: 700;
-  }
-  span:last-child {
+`;
+
+const PubOwner = styled.span`
+  ${(props) => props.theme.fontStyles.subFont1}
+  font-size:1.6rem;
+  line-height: 2.4rem;
+`;
+
+const PubPosition = styled.div`
+  display: flex;
+  align-items: center;
+  > span {
+    ${(props) => props.theme.fontStyles.subFont1}
+    font-weight:400;
     font-size: 1.4rem;
+    line-height: 2.2rem;
   }
+  div {
+    margin-right: 0.4rem;
+  }
+`;
+
+const PubPage = styled.div`
+  span {
+    color: ${(props) => (props.isPageOne ? '#ffffff' : '#0075FF')};
+    ${(props) => props.theme.fontStyles.subFont1}
+    font-weight:400;
+    font-size: 1.4rem;
+    line-height: 2.2rem;
+  }
+  padding: 0 0.4rem;
+  border-radius: 0.2rem;
+  background-color: ${(props) => (props.isPageOne ? '#0075ff' : '#D9EAFC')};
+`;
+
+const DevideCircle = styled.div`
+  width: 0.2rem;
+  height: 0.2rem;
+  border-radius: 50%;
+  background-color: #000000;
+`;
+
+const PubMainMenu = styled.div`
+  ${(props) => props.theme.fontStyles.subFont1}
+  font-weight:400;
+  font-size: 1.4rem;
+  line-height: 2.2rem;
+  color: #333;
+`;
+
+const PubIntroduce = styled.p`
+  ${(props) => props.theme.fontStyles.subFont1}
+  font-weight:400;
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+  color: #8e8e8e;
 `;
--- src/pages/booth/Pub.jsx
+++ src/pages/booth/Pub.jsx
@@ -10,28 +10,32 @@ export default function Pub() {
     {
       image: 'https://cdn.pixabay.com/photo/2023/04/27/10/22/cat-7954262_1280.jpg',
       owns: '예술학과 주점',
-      page: '1-1',
+      page: 1,
+      position: 1,
       mainMenu: '국물떡볶이',
       introduction: '가장 맛있는 주점입니다. 많이많이 놀러오세요!! 서비스 많이 드립니다.',
     },
     {
       image: 'https://cdn.pixabay.com/photo/2023/04/27/10/22/cat-7954262_1280.jpg',
       owns: '컴퓨터공학과 주점',
-      page: '1-2',
+      page: 1,
+      position: 2,
       mainMenu: '감자튀김',
       introduction: '가장 맛있는 주점입니다. 많이많이 놀러오세요!! 서비스 많이 드립니다.',
     },
     {
       image: 'https://cdn.pixabay.com/photo/2023/04/27/10/22/cat-7954262_1280.jpg',
       owns: '경영학과 주점',
-      page: '1-1',
+      page: 2,
+      position: 1,
       mainMenu: '닭꼬치',
       introduction: '가장 맛있는 주점입니다. 많이많이 놀러오세요!! 서비스 많이 드립니다.',
     },
     {
       image: 'https://cdn.pixabay.com/photo/2023/04/27/10/22/cat-7954262_1280.jpg',
       owns: '화학공학과 주점',
-      page: '1-2',
+      page: 2,
+      position: 2,
       mainMenu: '계란말이',
       introduction: '가장 맛있는 주점입니다. 많이많이 놀러오세요!! 서비스 많이 드립니다.',
     },
@@ -87,7 +91,7 @@ const PubMainMap = styled.img`
 const PubBooths = styled.div`
   display: flex;
   flex-direction: column;
-  width: 31.3rem;
+  width: 33.5rem;
 `;
 
 const AreaText = styled.div`
