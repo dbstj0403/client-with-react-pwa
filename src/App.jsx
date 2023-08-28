e02484c6402f68305d8ea3b9989876db7c03d50f
김진호 <rlawlsgh1227@gmail.com>
2023-08-28 16:13:26 +0900

    fix: app.js theme, provider 중복 선언으로 인해 생긴 충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
 import Home from './pages/Home';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyle';
-import theme from './styles/theme';
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
