7263ccecfaef5d0c74c7f2498f83c86418af71c7
brian <90752841+wokbjso@users.noreply.github.com>
2023-08-28 16:45:49 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
 import Home from './pages/Home';
-import { Home } from './pages/Home';
 import Facilities from './pages/Facilities';
 import Pub from './pages/booth/Pub';
 import FoodTruck from './pages/booth/FoodTruck';
