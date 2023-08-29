27c934a1c3f324fe02510c8a63e29f275a7eaef0
yongun2 <djkye0001@naver.com>
2023-08-29 12:38:36 +0900

    [FIX] import 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
-import { Home } from './pages/Home';
 import WowDigitalFestival from './pages/WowDigitalFestival';
 import Home from './pages/Home';
 import Facilities from './pages/Facilities';
