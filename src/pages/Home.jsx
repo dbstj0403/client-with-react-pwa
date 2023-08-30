9ca7f2019ae430445d5b25bc53851f9ecbfd3182
김진호 <rlawlsgh1227@gmail.com>
2023-08-30 22:24:43 +0900

    design: 스크롤 반영으로 인해 3컴포넌트로 변경
    아직 스크롤 기능은 구현하지 않았습니다

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Landing from './main/Landing';
 import Campus from './main/Campus';
-import RoadMap from './main/Roadmap';
-import Schedule from './main/Schedule';
+import Rest from './main/Rest';
 
 function Home() {
   return (
     <>
       <Campus />
       <Landing />
-      <RoadMap />
-      <Schedule />
+      <Rest />
     </>
   );
 }
