8eb741fa31f21fc7d7d9fe6577e1742912271472
김진호 <rlawlsgh1227@gmail.com>
2023-09-01 21:31:51 +0900

    feat: 스크롤 20부터 이미지 그라데이션 적용

--- src/pages/main/Campus.jsx
+++ src/pages/main/Campus.jsx
@@ -1,14 +1,34 @@
+import theme from '@/styles/theme';
 import styled from 'styled-components';
 
-function Campus() {
-  return <CampusImage src="/img/demo.png" alt="hongik" />;
+function Campus(props) {
+  return (
+    <Container fix={props.scroll > 20 ? 1 : 0}>
+      <Backdrop scroll={props.scroll} />
+      <CampusImage src="/img/demo.png" alt="hongik" />
+    </Container>
+  );
 }
 
 export default Campus;
 
+const Container = styled.div`
+  position: ${(props) => (props.fix ? 'fixed' : 'relative')};
+  top: ${(props) => (props.fix ? '-2.1rem' : 0)};
+  width: 100%;
+  height: 24rem;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 24rem;
+  background-color: ${theme.colors.background};
+  opacity: ${(props) => (props.scroll < 100 && props.scroll > 20 ? (props.scroll - 20) / 100 : 0)};
+`;
+
 const CampusImage = styled.img`
   width: 100%;
   height: 24rem;
-
   object-fit: cover;
 `;
