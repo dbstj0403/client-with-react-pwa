7ed2b3505be4f42c2ad93fceeb807cb16f7f2435
dbstj0403 <wcn09088@naver.com>
2023-09-06 23:20:25 +0900

    수정된 페이지 디자인 완성 및 aos 애니메이션

--- src/pages/WowDjFestival.jsx
+++ src/pages/WowDjFestival.jsx
@@ -2,27 +2,31 @@ import styled from 'styled-components';
 import React from 'react';
 import InfoCard from '@/components/wowDj/InfoCard';
 import LineUp from '@/components/wowDj/LineUp';
-import { useState } from 'react';
 import djFestivalMapImg from '../assets/image/djFestivalMapImg.png';
 import backgroundImg1 from '../assets/image/FestivalBackground.svg';
 import backgroundImg2 from '../assets/image/InfoCardBackground.svg';
 import MoveToTopBtn from '@/components/common/btn/MoveToTopBtn';
+import djFestivalPoster from '../assets/image/djFestivalPoster.svg';
 
 function WowDigitalFestival() {
-  const [isAuth, setIsAuth] = useState(false);
   return (
     <Container>
       <Title>와우 디제이 페스티벌</Title>
       <TitleInfo>
         와우 디제이 페스티벌은 <br />
-        다양한 매력을 가진 DJ들이 찾아와 <br /> 자신만의 음악을 사람들과 즐기는 페스티벌입니다. <br />
-        다양한 컨텐츠가 준비되어 있으니 많은 관심 부탁드립니다.
+        다양한 매력을 가진 DJ들이 찾아와 자신만의 <br />
+        음악을 사람들과 즐기는 페스티벌입니다. <br />
+        다양한 컨텐츠가 준비되어 있으니 <br />
+        많은 관심 부탁드립니다.
       </TitleInfo>
 
       <ImgWrapper>
         <ImgContainer>
-          <img src={djFestivalMapImg} alt="" className="mapImg" />
+          <img src={djFestivalPoster} alt="" />
         </ImgContainer>
+        <ImgContainer1>
+          <img src={djFestivalMapImg} alt="" />
+        </ImgContainer1>
         <ImgContainer2>
           <img src={backgroundImg1} alt="" />
         </ImgContainer2>
@@ -36,6 +40,9 @@ function WowDigitalFestival() {
       </ImgWrapper>
 
       <ImgWrapper>
+        <ImgContainer5>
+          <img src={backgroundImg2} alt="" />
+        </ImgContainer5>
         <LineUp />
         <ImgContainer4>
           <img src={backgroundImg2} alt="" />
@@ -78,7 +85,24 @@ const ImgContainer = styled.div`
   width: 33.5rem;
   height: 48.4rem;
   margin-top: 2rem;
+  margin-bottom: 10rem;
   margin: 0 auto;
+  margin-bottom: 3.5rem;
+  position: relative;
+  z-index: 5;
+  img {
+    width: 33.5rem;
+    height: 48.4rem;
+  }
+`;
+const ImgContainer1 = styled.div`
+  width: 33.5rem;
+  height: 48.4rem;
+  margin-top: 2rem;
+  margin: 0 auto;
+  margin-bottom: 3rem;
+  position: relative;
+  z-index: 5;
   img {
     width: 33.5rem;
     height: 48.4rem;
@@ -86,20 +110,21 @@ const ImgContainer = styled.div`
 `;
 const ImgContainer2 = styled.div`
   position: absolute;
-  bottom: -25.5rem;
+  bottom: 31rem;
   left: -5rem;
   width: 50rem;
   height: 40rem;
+  z-index: 2;
   img {
     width: 23.679rem;
     height: 29.317rem;
-    mix-blend-mode: exclusion;
   }
 `;
 const ImgContainer3 = styled.div`
   position: absolute;
-  bottom: -6rem;
+  bottom: 40rem;
   left: 15rem;
+  z-index: 2;
   img {
     width: 23.679rem;
     height: 29.317rem;
@@ -114,5 +139,19 @@ const ImgContainer4 = styled.div`
     height: 29.317rem;
     transform: rotate(80deg);
     mix-blend-mode: exclusion;
+  }
+`;
+const ImgContainer5 = styled.div`
+  position: absolute;
+  top: 14rem;
+  left: 6rem;
+  z-index: 2;
+  img {
+    width: 38.8rem;
+    height: 34.4rem;
+    transform: rotate(-40deg);
+
+    //transform: scaleX(-1);
+    //mix-blend-mode: exclusion;
   }
 `;
