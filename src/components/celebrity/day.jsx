251e9e40af7e04c6b674849233f26f04ebf77bd2
김진호 <rlawlsgh1227@gmail.com>
2023-09-07 09:47:17 +0900

    fix: 연예인 소개 부분 그라데이션 적용 및 차트맨 추가
    카드 별 그라데이션 배경 다르게 보이게 하는 부분은 아직 적용 못했습니다....

--- src/components/celebrity/day.jsx
+++ src/components/celebrity/day.jsx
@@ -9,8 +9,17 @@ function Day({ day, info }) {
 
       {info.map((celeb, idx) => (
         <Card key={`celeb-${day}-${idx}`}>
-          <CelebImage src={celeb.image} />
+          <CelebImageContainer>
+            <CelebImage src={celeb.image} />
+            {celeb.chartman ? (
+              <ChartMan>
+                본 아티스트는 <ChartManIcon src="/img/chartman.png" />과 함께합니다.
+              </ChartMan>
+            ) : null}
+          </CelebImageContainer>
+
           <Introduction>
+            <GraditionLayer />
             <Celeb>{celeb.name}</Celeb>
             <Desc>{celeb.introduce}</Desc>
           </Introduction>
@@ -44,12 +53,18 @@ const Card = styled.div`
   margin-bottom: 3.6rem;
 `;
 
+const CelebImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
 const CelebImage = styled.img`
   width: 100%;
   height: 21.6rem;
 `;
 
 const Introduction = styled.div`
+  position: relative;
   width: 100%;
   height: 15rem;
   padding: 1.8rem 2.8rem 2rem 2rem;
@@ -61,18 +76,51 @@ const Introduction = styled.div`
   background-repeat: no-repeat;
 `;
 
-const Celeb = styled.div`
-  color: ${theme.colors.white};
+const GraditionLayer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  background: linear-gradient(180deg, #000 0%, rgba(0, 0, 0, 0) 100%);
 
+  width: 100%;
+  height: 15rem;
+`;
+
+const Celeb = styled.div`
+  position: relative;
+  z-index: 2;
+  color: ${theme.colors.white};
   ${theme.fontStyles.head5};
 `;
 
 const Desc = styled.div`
-  padding: 1.2rem 0;
+  position: relative;
+  z-index: 2;
 
+  margin-top: 0.8rem;
+  padding: 1.2rem 0;
   border-top: 0.1rem solid ${theme.colors.white};
+  border-image: linear-gradient(to left, #ffffff00, ${theme.colors.white});
+  border-image-slice: 1;
 
   color: ${theme.colors.white};
   ${theme.fontStyles.subHead1};
   white-space: pre-line;
 `;
+
+const ChartMan = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+
+  width: 100%;
+  height: 4rem;
+  padding-left: 1.6rem;
+
+  color: ${theme.colors.pink};
+  ${theme.fontStyles.body5};
+`;
+
+const ChartManIcon = styled.img``;
