675f7b0c2a19964582378f15472231d473b9cdd9
brian <90752841+wokbjso@users.noreply.github.com>
2023-09-12 11:08:23 +0900

    Feat:주점 서브 이미지 렌더링 방식 수정

--- src/components/booth/PubCategory.jsx
+++ src/components/booth/PubCategory.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 export default function PubCategory({ categories, setCategoryText, setDepartment }) {
   const [categoryState, setCategoryState] = useState(0);
   const [pageSection, setPageSection] = useState('ALL');
+  const [subImage, setSubImage] = useState(null);
   const categoryClicked = (index) => {
     setCategoryState(index);
   };
@@ -17,6 +18,7 @@ export default function PubCategory({ categories, setCategoryText, setDepartment
             <span
               onClick={() => {
                 categoryClicked(index);
+                setSubImage(category.image);
                 setCategoryText(category.name === '디경융' ? '디지털경영융합' : category.name);
                 setPageSection(category.page);
                 setDepartment(category.engName);
@@ -27,7 +29,7 @@ export default function PubCategory({ categories, setCategoryText, setDepartment
           </CategoryTextBox>
         ))}
       </Categories>
-      <SubMapImage pageSection={pageSection} />
+      <SubMapImage image={subImage} />
     </PubCategoryWrapper>
   );
 }
@@ -47,24 +49,15 @@ const CategoryTextBox = styled.div`
   padding: 0.8rem 1.2rem;
   border-radius: 0.2rem;
   background-color: ${(props) => (props.isClicked ? props.category.backgroundColor : null)};
-  transition: all 0.25s ease;
+  transition: background-color 0.25s ease;
   span {
     ${(props) => props.theme.fontStyles.head5}
-    color:${(props) => (props.isClicked ? props.category.textColor : props.theme.colors.gray700)};
-    transition: all 0.25s ease;
-  }
+    color:${(props) => (props.isClicked ? props.category.textColor : props.theme.colors.gray700)}
 `;
 
 const SubMapImage = styled.div`
   width: 33.5rem;
   height: 33.5rem;
-  background: url(${(props) =>
-    props.pageSection === 'A'
-      ? '/img/pubSectionA.png'
-      : props.pageSection === 'B'
-      ? '/img/pubSectionB.png'
-      : props.pageSection === 'C'
-      ? '/img/pubSectionC.png'
-      : null});
+  background: url(${(props) => props.image});
   background-size: cover;
 `;
